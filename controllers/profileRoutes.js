a1c57cc41c6e77484ed0ea1c213a985e6720ea2f
JohnKihm <john.a.kihm@gmail.com>
2024-07-08 19:51:19 -0500

    added get profile route, displaying all jokes a user has posted

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,5 +1,23 @@
 const router = require('express').Router();
+const { DadJoke } = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const jokeData = await DadJoke.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        });
 
+        const jokes = jokeData.map((joke) => joke.get({ plain: true }));
+
+        res.render('profile', {
+            jokes
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
