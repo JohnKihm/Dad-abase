ff3da647a8a103bab61f35385e2aee9755a4aa5a
JohnKihm <john.a.kihm@gmail.com>
2024-07-08 19:04:34 -0500

    layed out controller relationships

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const jokeRoutes = require('./jokeRoutes');
+
+router.use('/users', userRoutes);
+router.use('/jokes', jokeRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
-//will create later
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const profileRoutes = require('./profileRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
+
+module.exports = router;
