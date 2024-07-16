e647c41d70d3cdacd663de1440997c9fe4659cd2
Ben Hill <benjaminjhill@icloud.com>
2024-07-15 19:41:22 -0500

    added new sounds for stars 1235

--- public/js/toneSounds.js
+++ public/js/toneSounds.js
@@ -4,37 +4,43 @@ const threeStar = document.getElementById("tone-btn-three");
 const fourStar = document.getElementById("tone-btn-four");
 const fiveStar = document.getElementById("tone-btn-five");
 
-const laughTrack = new Tone.Player("/assets/sounds-crowd.mp3").toDestination();
+
+const boo1 = new Tone.Player("/assets/Booing.mp3").toDestination();
+const crickets2 = new Tone.Player("/assets/Crickets.mp3").toDestination();
+const yay3 = new Tone.Player("/assets/Yay.mp3").toDestination();
+const laughTrack4 = new Tone.Player("/assets/sounds-crowd.mp3").toDestination();
+const lmao5 = new Tone.Player("/assets/Lmao.mp3").toDestination();
+
 const quackTrack = new Tone.Player("/assets/duck-quacking.mp3").toDestination();
 
-function toneSound(soundlvl) { //change values of soundlvl in evtlisteners if they are too quiet or loud
+function toneSound(soundlvl, sound) { //change values of soundlvl in evtlisteners if they are too quiet or loud
     if (Tone.context.state !== "running") {
         Tone.start();
     }
-    let egg = Math.floor((Math.random() * 1111) + 1)
+    let egg = Math.floor((Math.random() * 111) + 1)
     if (egg === 111) {
         quackTrack.volume.value = soundlvl;
         quackTrack.start();
     } else {
-        laughTrack.volume.value = soundlvl;
-        laughTrack.start();
+        sound.volume.value = soundlvl;
+        sound.start();
     }
 }
 
 if (oneStar) {
     oneStar.addEventListener("click", () => {
-        toneSound(-15);
+        toneSound(-10, boo1);
     });
     twoStar.addEventListener("click", () => {
-        toneSound(-11);
+        toneSound(-8, crickets2);
     });
     threeStar.addEventListener("click", () => {
-        toneSound(-7);
+        toneSound(-5, yay3);
     });
     fourStar.addEventListener("click", () => {
-        toneSound(-3);
+        toneSound(-3, laughTrack4);
     });
     fiveStar.addEventListener("click", () => {
-        toneSound(0);
+        toneSound(0, lmao5);
     });
 }
